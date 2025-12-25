bd6fa790a96d4fc16517aca33497ef50c3fcf195
angelogiu <angelogiustino@icloud.com>
2025-12-24 23:25:19 -0800

    Started organizing data

--- download_data.py
+++ download_data.py
@@ -1,1 +1,12 @@
-#Download the data from a certain stock 
+import yfinance as yf
+import pandas as pd
+
+
+df = yf.download("SPY", start="2015-01-01", end="2025-01-01")
+df = df.sort_index()
+df.reset_index().to_csv("data/raw/SPY.csv", index=False)
+
+df['returns'] = (df['Close'] - df['Close'].shift(1)) / df['Close'].shift(1)
+
+df.reset_index().to_csv("data/processed/SPY.csv", index=False)
+print(df)
